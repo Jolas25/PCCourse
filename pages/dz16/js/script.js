ba92a2852a756f06e4ae6191a92039cbacc1fcc4
Jolas25 <oktant2012@gmail.com>
2020-12-08 15:26:12 +0200

    dz16

---
+++ pages/dz16/js/script.js
@@ -0,0 +1,20 @@
+let a = prompt('a= ');
+let b = prompt('b= ');
+let c = a * b;
+alert(a * b);
+
+
+let name = prompt('Имя: ');
+let surName = prompt('Фамилия: ');
+let performance = prompt('Представление: ');
+let theatre = prompt('Театр: ');
+let sector = prompt('Сектор: ');
+let row = prompt('Ряд: ');
+let seat = prompt('Место: ');
+let time = prompt('Время: ');
+
+let ticket = `Имя: ${name} \nФамилия: ${surName} \nПредставление: ${performance} \nТеатр: ${theatre} \nСектор: ${sector} \nРяд: ${row} \nМесто: ${seat} \nВремя: ${time}`;
+
+console.log(ticket);
+console.log('a * b = ' + c);
+
